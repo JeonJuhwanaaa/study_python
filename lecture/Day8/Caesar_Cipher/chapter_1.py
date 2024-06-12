e00869785ab1c3386db8a45de4d8cb202b7adaa0
JeonJuhwanaaa <w7285@naver.com>
2024-06-12 12:02:32 +0900

    fix:python_Day8

--- lecture/Day8/Caesar_Cipher/chapter_1.py
+++ lecture/Day8/Caesar_Cipher/chapter_1.py
@@ -1,6 +1,9 @@
-# << 카이사르 암호 챕터 1 - 암호화 >>
+# << 카이사르 암호 챕터 1 - 암호화 / encode >>
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
--- lecture/Day8/Caesar_Cipher/chapter_3.py
+++ lecture/Day8/Caesar_Cipher/chapter_3.py
@@ -1,1 +1,46 @@
-# << 카이사르 암호 챕터 3 - 코드 재구성 >>
+# << 카이사르 암호 챕터 3 - 코드 재구성 / 코드를 더 간결하게 >>
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
+
+# << 위 두 def 코드 합치기 >> 
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+
+  if cipher_direction == "decode":
+    shift_amount *= -1
+
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"The {cipher_direction}d text is {end_text}")
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
