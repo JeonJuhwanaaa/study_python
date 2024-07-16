1f5238c979797c93641ecbc34a9a7eef4eaed28d
JeonJuhwanaaa <w7285@naver.com>
2024-07-16 17:14:14 +0900

    feat:pycharm_Day31:Flash_Card_Project

--- ptcharm/Day31_Flash_card_project/main.py
+++ ptcharm/Day31_Flash_card_project/main.py
@@ -1,37 +1,76 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    # print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+    # print(to_learn)
 
+def next_card():
+    global current_card, flip_timer
+    # 카드 뒤집기 초기화
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    # print(current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    # 카드 3초 뒤에 뒤집기.
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
+
+def is_known():
+    to_learn.remove(current_card)
+    # print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
 
+# 3초 뒤 func 실행
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+
 card_front_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_image)
+
+card_title = canvas.create_text(400, 150, text="title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+
 canvas.grid(column=0 ,row=0, columnspan=2)
 
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
-# -------------------------------------------Button-------------------------------------------
+# ------------------------------------------- Button -------------------------------------------
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(column=1, row=1)
 
 
-
-
-
-
-
-
+next_card()
 
 window.mainloop()
