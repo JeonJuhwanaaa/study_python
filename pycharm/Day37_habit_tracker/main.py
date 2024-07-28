78c9aa57ba04c12024c3f735f49b48f8ede4eeeb
JeonJuhwanaaa <w7285@naver.com>
2024-07-29 00:08:31 +0900

    feat:pycharm_Day37:habit_tracker_requests

--- pycharm/Day37_habit_tracker/main.py
+++ pycharm/Day37_habit_tracker/main.py
@@ -1,6 +1,11 @@
 import requests
 from datetime import datetime
 
+# https://pixe.la/v1/users/juhwan/graphs/graph1.html
+
+# api 가져오기.
+
+# 임의로 지정
 USERNAME = "juhwan"
 TOKEN = "abmlkgnbl324kowf2k34"
 GRAPH_ID = "graph1"
@@ -15,6 +20,10 @@ user_params = {
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+# ------------------------------------------------------------------------------------------
+
+# header 로 authentication(인증)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
@@ -33,21 +42,42 @@ headers = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
-# https://pixe.la/v1/users/juhwan/graphs/graph1.html
+# ------------------------------------------------------------------------------------------
+
+# 날짜 / km 수 데이터 입력 넣기.
 
 pixel_create_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-# today = datetime.now()
+today = datetime.now()
 # print(today.strftime("%y%m%d"))
-today = datetime(year=2024, month=7, day=27)
-
+# today = datetime(year=2024, month=7, day=27)
 
 pixel_data = {
     # "date": "20240728",
-    "date": today.strftime("%y%m%d"),
-    "quantity": "15"
+    "date": today.strftime("%Y%m%d"), # %Y -> 년도(Y 대문자) / %m -> 달 / %d -> 날
+    "quantity": input("How many kilometers did you cycle today? ")
 }
 
-# response = requests.post(url=pixel_create_endpoint, json=pixel_data, headers=headers)
+response = requests.post(url=pixel_create_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+# ------------------------------------------------------------------------------------------
+
+# PUT 요청
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
 # print(response.text)
 
+# ------------------------------------------------------------------------------------------
+
+# Delete 요청
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
